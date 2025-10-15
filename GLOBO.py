482c6f1eb53c9a2c3c05478443cc3488ab54a4f2
pgmtv <programasexclusivos3@gmail.com>
2025-10-15 16:49:21 -0300

    Update GLOBO.py

--- GLOBO.py
+++ GLOBO.py
@@ -18,7 +18,6 @@ globoplay_urls = [
     "https://globoplay.globo.com/ao-vivo/7689934/",
     "https://globoplay.globo.com/ao-vivo/7690141/",    
     "https://globoplay.globo.com/v/12749215/",
-    "https://ge.globo.com/sportv/video/sportv-2-ao-vivo-no-geglobo-fifa-monitoracao-7690141.ghtml",
     "https://g1.globo.com/rr/roraima/video/ao-vivo-assista-o-jornal-de-roraima-1a-edicao-2923545-1739458038240.ghtml",
     "https://g1.globo.com/sp/ribeirao-preto-franca/ao-vivo/bom-dia-cidade-ribeirao-preto.ghtml",  # Bom Dia Cidade Ribeirão Preto
     "https://g1.globo.com/sp/ribeirao-preto-franca/ao-vivo/eptv1.ghtml",  # EPTV 1ª Edição - Ribeirão Preto
